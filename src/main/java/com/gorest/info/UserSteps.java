693bfa1fc8bcfd33fc26a169b8f860b4ac7174f1
stester2211 <stester246@gmail.com>
2023-08-08 20:37:56 -0400

    Gorest Serenity done

---
+++ src/main/java/com/gorest/info/UserSteps.java
@@ -0,0 +1,86 @@
+package com.gorest.info;
+
+import com.gorest.constants.EndPoints;
+import com.gorest.model.UserPojo;
+import io.restassured.response.ValidatableResponse;
+import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Step;
+
+public class UserSteps
+{
+
+    static final String token = "Bearer 4629a5b5b2eb0ae06bde7dd5372a9433696a38e4342483cd11eaf201114c088e";
+
+    @Step("Creating new user record with name: {0}, email: {1}, gender: {2} and status: {3}")
+    public ValidatableResponse createUserRecord(String name, String email, String gender, String status)
+    {
+        UserPojo userPojo = new UserPojo();
+        userPojo.setName(name);
+        userPojo.setEmail(email);
+        userPojo.setGender(gender);
+        userPojo.setStatus(status);
+        return SerenityRest.given().log().all()
+                .header("Authorization", token)
+                .header("Content-Type", "application/json")
+                .header("Connection", "keep-alive")
+                .body(userPojo)
+                .when()
+                .post("/users")
+                .then().log().all().statusCode(201);
+    }
+
+    @Step("Getting all users records")
+    public ValidatableResponse getUserIDs()
+    {
+        return SerenityRest.given().log().all()
+                .header("Authorization", token)
+                .header("Connection", "keep-alive")
+                .when()
+                .get(EndPoints.GET_ALL_USER)
+                .then().log().all().statusCode(200);
+    }
+
+    @Step("Updating user record with id: {0}, name: {1}, email: {2}, gender: {3} and status: {4}")
+    public void userRecordUpdate(int id, String name, String email, String gender, String status)
+    {
+        UserPojo userPojo = new UserPojo();
+        userPojo.setName(name);
+        userPojo.setEmail(email);
+        userPojo.setGender(gender);
+        userPojo.setStatus(status);
+        SerenityRest.given().log().all()
+                .header("Authorization", token)
+                .header("Content-Type", "application/json")
+                .header("Connection", "keep-alive")
+                .pathParam("id", id)
+                .body(userPojo)
+                .when()
+                .put(EndPoints.UPDATE_USER_BY_ID)
+                .then().log().all().statusCode(200);
+    }
+
+    @Step("Getting single record with id: {0}")
+    public ValidatableResponse getSingleUser(int id)
+    {
+        return SerenityRest.given().log().all()
+                .header("Authorization", token)
+                .header("Connection", "keep-alive")
+                .pathParam("id", id)
+                .when()
+                .get(EndPoints.GET_SINGLE_USER_BY_ID)
+                .then().log().all();
+    }
+
+    @Step("Deleting  user record with id: {0}")
+    public ValidatableResponse deleteUserRecord(int id)
+    {
+        return SerenityRest.given().log().all()
+                .header("Authorization", token)
+                .header("Connection", "keep-alive")
+                .pathParam("id", id)
+                .when()
+                .delete(EndPoints.DELETE_USER_BY_ID)
+                .then().log().all();
+    }
+
+}
